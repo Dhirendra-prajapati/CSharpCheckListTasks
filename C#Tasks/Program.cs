6e98cc7fdc7845c1dae7e95edb316d5472718431
Dhirendra-Prajapati <dhirendra@nimapinfotech.com>
2023-10-31 09:20:01 +0530

    demonstrate constructor

--- C#Tasks/Program.cs
+++ C#Tasks/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace C_Tasks
 {
+    public class Student
+    {
+        public int id;
+        public string name;
+      public Student(int id,string name)
+        {
+            this.id = id;
+            this.name = name;
+            Console.WriteLine("Constructor invoke");
+
+        }
+        public void display()
+        {
+            Console.WriteLine(id +" "+name);
+        }
+    }
     internal class Program
     {
+
         static void Main(string[] args)
         {
+            var c= new Student(12,"dhirendra");
+            c.display();
+            Console.ReadLine();
         }
     }
 }
