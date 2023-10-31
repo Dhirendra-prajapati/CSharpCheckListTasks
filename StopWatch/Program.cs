4d264a2d99bdf7c99b0dada846b15adf1c102454
Dhirendra-Prajapati <dhirendra@nimapinfotech.com>
2023-10-31 18:05:35 +0530

    StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class Stopwatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running;
+        public void Start()
+        {
+            if (_running)
+                throw new InvalidOperationException("StopWatch is already running");
+            _startTime = DateTime.Now;
+            _running = true;
+        }
+        public void Stop()
+        {
+            if (!_running)
+                throw new InvalidOperationException("StopWatch is not Running");
+            _endTime = DateTime.Now;
+            _running = false;
+
+        }
+        public TimeSpan GetInterval()
+        {
+            return _startTime - _endTime;
+        }
+    }
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            for (int i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+                Thread.Sleep(1000);
+                stopwatch.Stop();
+                Console.WriteLine("duration :" + stopwatch.GetInterval());
+                Console.WriteLine("press enter to run the stopwatch one more time");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Unit_Testing/ILogger1.cs
@@ -0,0 +1,8 @@
+﻿namespace Unit_Testing
+{
+    public interface ILogger1
+    {
+        void LogError(string message);
+        void LogInfo(string message);
+    }
+}
