c4bce3c7bf5b6efec292a468a68a23fba7fa609e
Dhirendra-Prajapati <dhirendra@nimapinfotech.com>
2023-11-01 12:33:54 +0530

    stringbuilder task

---
+++ Practice_problem/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_problem
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("cannot add a null object in the stack");
+            _list.Add(obj);
+
+        }
+        public object Pop()
+        {
+            if (_list.Count == 0)
+                throw new InvalidOperationException("There are no element in the stack");
+            var index = _list.Count - 1;
+            var toReturn = _list[index];
+            _list.RemoveAt(index);  
+            return (toReturn);
+
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            int ini = 10;
+            int max = 12;
+            for (int i = ini; i <= max; i++)
+                stack.Push(i);
+            for (int i = ini; i <= max; i++)
+                Console.WriteLine( stack.Pop());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Stringbuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringbuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append('_', 10);
+            sb.AppendLine();
+            sb.Append("header");
+            sb.AppendLine();
+            sb.Append('_', 10);
+            sb.Replace('_', '+');
+            sb.Remove(0, 10);
+            sb.Insert(0, new string('_', 10));
+            Console.WriteLine(sb);
+            Console.WriteLine("first char:"+ sb[0]);
+            Console.ReadLine();
+        }
+    }
+}
