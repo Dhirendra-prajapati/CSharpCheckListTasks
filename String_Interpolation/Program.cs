543b6a2a6ef553761285ac71931a25df52c553e3
Dhirendra-Prajapati <dhirendra@nimapinfotech.com>
2023-11-07 16:51:50 +0530

    string , stringbuilder and string interpolation

---
+++ String_Interpolation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Interpolation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string
+            string s1 = "hello";
+            string s2 = "world";
+            Console.WriteLine(s1+" "+s2);
+            //string builder
+            StringBuilder sb=new StringBuilder();
+            sb.Append('_', 10);
+            sb.AppendLine();
+            sb.Append("header");
+            Console.WriteLine(sb);
+
+            //string interpolation
+            int n1 = 10;
+            int n2 = 20;
+            string s = $"The sum of {n1} and {n2} is {n1 + n2}";
+            Console.WriteLine(s);
+
+
+
+            Console.ReadLine(); 
+        }
+    }
+}
---
+++ practical/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace practical
+{
+    internal class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            Func<int, int> square = number => number * number;
+            Console.WriteLine("The square of number"+square(5));
+            const int factor = 5;
+            Func<int, int> multipler = number => number * factor;
+            int result = multipler(10);
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
